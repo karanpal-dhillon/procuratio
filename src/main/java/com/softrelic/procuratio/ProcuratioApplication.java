6843311261194cdb5d29d317bc75794c79c87644
Karan Dhillon <Karanpal.dhillon@gmail.com>
2024-12-10 09:07:50 +0530

    first commit

---
+++ src/main/java/com/softrelic/procuratio/ProcuratioApplication.java
@@ -0,0 +1,13 @@
+package com.softrelic.procuratio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcuratioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcuratioApplication.class, args);
+	}
+
+}
